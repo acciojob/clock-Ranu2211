112062bab468d7997809a499d38c459a3125f481
Ranu <88536226+Ranu2211@users.noreply.github.com>
2025-01-18 19:31:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let timer = document.getElementById('timer');
+let CurrDate = new Date();
+setInterval(
+	function(){
+		CurrDate = new Date();
+		timer.innerHTML = CurrDate.toLocaleString();
+	},1000
+);
